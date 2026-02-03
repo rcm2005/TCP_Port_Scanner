0fdd2494bb9c974b01d8d35b1d605e505f1db378
Rafael Carvalho Mattos <rcm@Terminal.local>
2026-02-03 01:20:29 -0300

    Adding first commit: Starting ScanPort function

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Educational port scanner by Rafael M.
+using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+class PortScanner
+{
+    static async Task ScanPort(string host, int port, int timeoutMs = 500):
+    {
+        using var client = new TcpClient();
+
+        try
+        {
+            var connectTask = client.ConnectAsync(host, port);
+            var timeoutTask = Task.Delay(timeout)
+        }
+    }
+}
